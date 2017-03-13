a5100c97bed60f4baada4c0bcb807110ae39d051
Kenneth <unusualkid@gmail.com>
2017-03-12 18:02:56 -0700

    Create KO observables

--- js/app.js
+++ js/app.js
@@ -1,37 +1,69 @@
-    var map;
 
-    var parent = this;
-    function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7413549, lng: -73.9980244},
-          zoom: 13,
-          mapTypeId: 'roadmap'
-        });
-        var tribeca = {lat: 40.7413549, lng: -73.9980244};
-        var marker = new google.maps.Marker({
-          position: tribeca,
-          map: map,
-          title: 'First Marker',
-          animation: google.maps.Animation.DROP,
-          draggable: true,
-          label: '1',
-          icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-        });
-        var infowindow = new google.maps.InfoWindow({
-          content: 'Do you ever feel like an InfoWindow?'
-        });
-        marker.addListener('click', function(){
-          infowindow.open(map, marker);
-          toggleBounce(marker=marker);
-        })
-      }
 
-      function toggleBounce(marker) {
-        console.log("here");
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-      }
+/* ======= ViewModel ======= */
+function viewModel (){
+  var self = this;
+  self.city = ko.observable("New York");
+  self.broth = ko.observable("Tonkotsu");
+  self.locations = ko.observableArray();
+  // var fsUrlSearch = "https://api.foursquare.com/v2/venues/explore?";
+  // Call Foursquare API to populate our data model
+  // fsUrlSearch = "https://api.foursquare.com/v2/venues/explore?";
+  // var  fsUrlSearch = "https://api.foursquare.com/v2/venues/explore?";
+  // fsUrlSearch += $.param({
+  //   'client_id': "ECAQTFZ5BBRCA2SUHXOG1CJDRF442M5N3MKZGC5KAI5GZYPW",
+  //   'client_secret': "4V25QGAAULY2DN2CH4KYQELGQ1PXXMNZBOZIQG0FKETPJL1B",
+  //   'v': getTodaysDate(),
+  //   'near': city,
+  //   'limit': 50,
+
+  // });
+  // fsUrlSearch = "https://api.foursquare.com/v2/venues/explore?" + $.param({
+  //   'client_id': "ECAQTFZ5BBRCA2SUHXOG1CJDRF442M5N3MKZGC5KAI5GZYPW",
+  //   'client_secret': "4V25QGAAULY2DN2CH4KYQELGQ1PXXMNZBOZIQG0FKETPJL1B",
+  //   'v': getTodaysDate(),
+  //   'limit': 50,
+  //   'near': city,
+  // });
+
+  self.fsUrlSearch = ko.computed(function() {
+    return "https://api.foursquare.com/v2/venues/explore?" + $.param({
+    'client_id': "ECAQTFZ5BBRCA2SUHXOG1CJDRF442M5N3MKZGC5KAI5GZYPW",
+    'client_secret': "4V25QGAAULY2DN2CH4KYQELGQ1PXXMNZBOZIQG0FKETPJL1B",
+    'v': getTodaysDate(),
+    'limit': 50,
+    'near': self.city,
+    })
+  });
+  console.log(self.fsUrlSearch);
+
+
+
+  // $.getJSON(fsUrlSearch, function(data) {
+  //   console.log(data);
+  //   for (i = 0 ; i < data.response.venues.length; i++) {
+  //     self.locations.push(data.response.venues[i]);
+  //   }
+  //   // self.locations = data.response.venues;
+  //   console.log(self.locations);
+  // }).fail(function(){
+  //   console.log('Foursquare API Could Not Be Loaded.');
+  // });
+};
+
+ko.applyBindings(new viewModel());
+
+
+
+
+
+// Return today's date in "YYYYMMDD" format for Foursquare API
+function getTodaysDate() {
+  var d = new Date();
+  var twoDigitMonth = (d.getMonth()+1).toString().length === 1 ?
+    "0" + (d.getMonth()+1).toString() : (d.getMonth()+1).toString();
+  var twoDigitDate = d.getDate().toString().length === 1 ?
+    "0" + d.getDate().toString() : d.getDate().toString();
+  return d.getFullYear().toString() + twoDigitMonth + twoDigitDate;
+}
+
---
+++ unused.js
@@ -0,0 +1,43 @@
+var map;
+
+function initMap(city) {
+    // Constructor creates a new map - only center and zoom are required.
+    var city = ko.observable{
+      lat: 35.689488,
+      lng: 139.691706
+    };
+
+    // Set map to the city inputted by user.
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: city,
+      zoom: 12,
+      mapTypeId: 'roadmap'
+    });
+    var tribeca = {lat: 35.689488,
+      lng: 139.691706};
+    var marker = new google.maps.Marker({
+      position: tribeca,
+      map: map,
+      title: 'First Marker',
+      animation: google.maps.Animation.DROP,
+      draggable: true,
+      label: '1',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: 'Do you ever feel like an InfoWindow?'
+    });
+    marker.addListener('click', function(){
+      infowindow.open(map, marker);
+      toggleBounce(marker=marker);
+    })
+  }
+
+function toggleBounce(marker) {
+    console.log("here");
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
